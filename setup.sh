50aba46fe03c562292469069a3bd4e5a9d0d0364
CyberPuck <traverskyle9@gmail.com>
2025-08-20 19:46:31 -0500

    Adding nerd fonts, updating from  to  to exclude git and github folders

--- setup.sh
+++ setup.sh
@@ -11,7 +11,12 @@ sudo apt install -y ripgrep fd-find
 sudp apt install -y xclip
 
 # Install Nerd Font
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/DroidSansMono.zip
+sudo unzip DroidSansMono.zip -d /usr/local/share/fonts/DroidSansMono/
+rm DroidSansMono.zip
+
+# Install Extra Font
 sudo apt install -y fonts-noto-color-emoji
 
 # Copy to config folder
-cp -r $PWD/. $HOME/.config/nvim/
+rsync $PWD/. $HOME/.config/nvim/ --exclude .git*
